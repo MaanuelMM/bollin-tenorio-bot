0ef74acf9216ed7397174d80dd720fff57567076
MaanuelMM <manuel.martin.malagon@alumnos.upm.es>
2019-02-17 23:58:16 +0100

    Change to Webhook and add server path for Cloud hosting

--- bot.py
+++ bot.py
@@ -1,13 +1,14 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-# Authors:      CoreDumped-ETSISI, MaanuelMM
-# Credits:      CoreDumped-ETSISI, python-telegram-bot, MaanuelMM
+# Authors:      MaanuelMM
+# Credits:      python-telegram-bot, CoreDumped-ETSISI, Eldinnie
 # Created:      2019/02/14
-# Last update:  2019/02/16
+# Last update:  2019/02/17
 
 import telegram
 import logger
 import datetime
+import os
 
 from data_loader import DataLoader
 from logger import get_logger
@@ -19,18 +20,18 @@ def error_callback(bot, update, error):
     try:
         raise error
     except Unauthorized:
-        logger.exception("remove update.message.chat_id from conversation list")
+        logger.exception("Remove update.message.chat_id from conversation list.")
     except BadRequest as e:
-        if update.message.chat_id < 0 and e == "Message can't be deleted":  # This pre-check is necessary if we do not want to spam the logs with "BadRequest: Message can't be deleted" as this bot has no power to remove user messages in private chats.
-            logger.exception("handle malformed requests - read more below!")
+        if update.message.chat_id < 0 and e == "Message can't be deleted.":  # This pre-check is necessary if we do not want to spam the logs with "BadRequest: Message can't be deleted" as this bot has no power to remove user messages in private chats.
+            logger.exception("Handle malformed requests - read more below!")
     except TimedOut:
-        logger.exception("handle slow connection problems")
+        logger.exception("Handle slow connection problems.")
     except NetworkError:
-        logger.exception("handle other connection problems")
+        logger.exception("Handle other connection problems.")
     except ChatMigrated as e:
-        logger.exception("the chat_id of a group has changed, use " + e.new_chat_id + " instead")
+        logger.exception("The chat_id of a group has changed, use " + e.new_chat_id + " instead.")
     except TelegramError:
-        logger.exception("There is some error with Telegram")
+        logger.exception("There is some error with Telegram.")
 
 def load_settings():
     global settings
@@ -38,56 +39,42 @@ def load_settings():
     settings = DataLoader()
     last_function_calls = {}
 
-def is_call_available(name, chat_id, cooldown):
-    global last_function_calls
-    now = datetime.datetime.now()
-    cooldown_time = datetime.datetime.now() - datetime.timedelta(minutes=cooldown)
-    if name in last_function_calls.keys():
-        if chat_id in last_function_calls[name].keys():
-            if last_function_calls[name][chat_id] > cooldown_time:
-                last_function_calls[name][chat_id] = now
-                return False
-            else:
-                last_function_calls[name][chat_id] = now
-                return True
-        else:
-            last_function_calls[name][chat_id] = now
-            return True
-    else:
-        last_function_calls[name] = {chat_id: now}
-        return True
-
 def log_message(update):
     try:
         username = update.message.from_user.username
     except:
-        username = "desconocido"
+        username = "Unknown"
     try:
         text = update.message.text
     except:
-        text = "algo"
-    logger.info("He recibido: \"" + text + "\" de " + username + " [ID: " + str(update.message.chat_id) + "]")
+        text = "something"
+    logger.info("Received: \"" + text + "\" from " + username + " [Chat ID: " + str(update.message.chat_id) + "]")
 
 def hola_command(bot, update):
-    if is_call_available("hola_mundo", update.message.chat_id, 180):
-        log_message(update)
-        bot.sendMessage(update.message.chat_id, settings.hola_mundo, parse_mode=telegram.ParseMode.HTML)
+    update.effective_message.reply_text(settings.hola_command)
 
 if __name__ == "__main__":
-    print("Bollín Tenorio Bot: Starting...")
-    
+    # Enable logging
     logger = get_logger("bot_starter", True)
-    load_settings()
-
+    logger.info("Starting...")
     try:
-        logger.info("Realizando conexión con la API de Telegram.")
+        # Load data and settings
+        load_settings()
+        # Set up the Updater
+        logger.info("Making connection with the Telegram API.")
         updater = Updater(settings.telegram_token)
+        # Add handlers
         dispatcher = updater.dispatcher
         dispatcher.add_handler(CommandHandler('hola', hola_command))
         dispatcher.add_error_handler(error_callback)
+        # Start the webhook
+        updater.start_webhook(listen="0.0.0.0", port=int(settings.server_port), url_path=settings.telegram_token)
+        updater.bot.setWebhook("{}{}".format(settings.server_url, settings.server_port))
     except:
-        logger.info("Error al realizar la conexión con la API de Telegram.")
+        # Shut down
+        logger.info("ERROR making connection with the Telegram API. Shutting down.")
         quit()
-    updater.start_polling()
-    logger.info("Bollín Tenorio Bot: Listening...")
-    updater.idle()
+    else:
+        # Successful start
+        logger.info("Listening...")
+        updater.idle()
--- data_loader.py
+++ data_loader.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-# Authors:      CoreDumped-ETSISI, MaanuelMM
-# Credits:      CoreDumped-ETSISI, MaanuelMM
+# Authors:      MaanuelMM, CoreDumped-ETSISI
+# Credits:      CoreDumped-ETSISI
 # Created:      2019/02/14
-# Last update:  2019/02/14
+# Last update:  2019/02/17
 
 from logger import get_logger
 import json
+import os
 
 logger = get_logger("data_loader")
 
@@ -21,8 +22,10 @@ class DataLoader:
             data_and_settings = private_data.copy()
             data_and_settings.update(data)
         except:
-            logger.exception("Error al cargar el JSON de configuración")
+            logger.exception("Failed to load the configuration JSON file.")
         else:
-            logger.info("JSON cargado con éxito")
+            logger.info("Successfully loaded the configuration JSON file.")
             self.telegram_token = data_and_settings["telegram_token"]
-            self.hola_mundo = data_and_settings["hola_mundo"]
+            self.server_url = data_and_settings["server_url"]
+            self.server_port = os.environ.get('PORT') # Getting the port number directly asking to the server
+            self.hola_command = data_and_settings["hola_command"]

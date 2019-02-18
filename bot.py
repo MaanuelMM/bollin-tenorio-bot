7688e70acc33a228d69dac7728a3581a30399700
MaanuelMM <manuel.martin.malagon@alumnos.upm.es>
2019-02-18 17:51:01 +0100

    Change Telegram API framework and delete JSON data files

--- bot.py
+++ bot.py
@@ -1,83 +1,66 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-# Authors:      MaanuelMM, CoreDumped-ETSISI
-# Credits:      python-telegram-bot, CoreDumped-ETSISI, Eldinnie
+# Authors:      MaanuelMM
+# Credits:      eternnoir, atlink, CoreDumped-ETSISI, Eldinnie
 # Created:      2019/02/14
 # Last update:  2019/02/18
 
-import telegram
-import logger
+import os
+import telebot
 import datetime
+import logger
 
-from data_loader import DataLoader
+from flask import Flask, request
 from logger import get_logger
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
-from telegram.error import TelegramError, Unauthorized, BadRequest, TimedOut, ChatMigrated, NetworkError
 
 
-def load_settings():
-    global settings
-    global last_function_calls
-    settings = DataLoader()
-    last_function_calls = {}
+# Enable logger
+logger = get_logger("bot_starter", True)
+logger.info("Starting...")
+# Get Congif Vars
+TOKEN = os.environ.get('TOKEN')
+URL = os.environ.get('URL')
+PORT = os.environ.get('PORT', 5000)
+try:
+    # Telegram API connection
+    logger.info("Connecting to the Telegram API...")
+    bot = telebot.TeleBot(token=TOKEN)
+    logger.info("Successfully connected to the Telegram API.")
+except:
+    logger.info("Error connecting to the Telegram API. Shutting down...")
+    quit()
+else:
+    # Start Flask app
+    server = Flask(__name__)
 
-def log_message(update):
-    try:
-        username = update.message.from_user.username
-    except:
-        username = "Unknown"
-    try:
-        text = update.message.text
-    except:
-        text = "something"
-    logger.info("Received: \"" + text + "\" from " + username + " [Chat ID: " + str(update.message.chat_id) + "].")
 
-def hola_command(bot, update):
-    update.effective_message.reply_text(settings.hola_command)
+@bot.message_handler(commands=['start']) # welcome message handler
+def send_welcome(message):
+    bot.reply_to(message, "Hola a todos, mis papirrines <3")
 
-def echo(bot, update):
-    update.effective_message.reply_text(update.effective_message.text)
+@bot.message_handler(commands=['help']) # help message handler
+def send_help(message):
+    bot.reply_to(message, "Lo siento si funciono mal o estoy muy limitado en funciones, pero estoy trabajando para mejorar día a día :)")
 
-def error_callback(bot, update, error):
-    try:
-        raise error
-    except Unauthorized:
-        logger.exception("Remove update.message.chat_id from conversation list.")
-    except BadRequest as e:
-        if update.message.chat_id < 0 and e == "Message can't be deleted.":  # This pre-check is necessary if we do not want to spam the logs with "BadRequest: Message can't be deleted" as this bot has no power to remove user messages in private chats.
-            logger.exception("Handle malformed requests - read more below!")
-    except TimedOut:
-        logger.exception("Handle slow connection problems.")
-    except NetworkError:
-        logger.exception("Handle other connection problems.")
-    except ChatMigrated as e:
-        logger.exception("The chat_id of a group has changed, use " + e.new_chat_id + " instead.")
-    except TelegramError:
-        logger.exception("There is some error with Telegram.")
+
+@server.route('/' + TOKEN, methods=['POST'])
+def getMessage():
+    bot.process_new_updates([telebot.types.Update.de_json(request.stream.read().decode("utf-8"))])
+    return "!", 200
+
+@server.route("/")
+def webhook():
+    bot.remove_webhook()
+    bot.set_webhook(url=URL+TOKEN)
+    return "!", 200
+
 
 if __name__ == "__main__":
-    # Enable logging
-    logger = get_logger("bot_starter", True)
-    logger.info("Starting...")
     try:
-        # Load data and settings
-        load_settings()
-        # Set up the Updater
-        logger.info("Connecting to the Telegram API...")
-        updater = Updater(settings.telegram_token)
-        # Add handlers
-        dispatcher = updater.dispatcher
-        dispatcher.add_handler(CommandHandler('hola', hola_command))
-        dispatcher.add_handler(MessageHandler(Filters.text, echo))
-        dispatcher.add_error_handler(error_callback)
-        # Start the webhook
-        updater.start_webhook(listen="0.0.0.0", port=int(settings.server_port), url_path=settings.telegram_token)
-        updater.bot.setWebhook(settings.server_url + settings.telegram_token)
-    except:
-        # Shut down
-        logger.info("ERROR making connection to the Telegram API. Shutting down...")
-        quit()
-    else:
-        # Successful start
+        # run server
+        logger.info("Starting running server...")
+        server.run(host="0.0.0.0", port=int(PORT))
         logger.info("Listening...")
-        updater.idle()
+    except:
+        logger.info("Error starting running server. Shutting down...")
+        quit()

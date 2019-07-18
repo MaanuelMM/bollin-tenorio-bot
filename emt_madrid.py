a775e6feb2d6ebb70c7a432e3781833332d3cc36
MaanuelMM <manuel.martin.malagon@alumnos.upm.es>
2019-07-18 10:23:21 +0200

    Reduce timeout for requests

--- emt_madrid.py
+++ emt_madrid.py
@@ -3,22 +3,22 @@
 # Authors:      MaanuelMM
 # Credits:      EMT-Madrid, alvaroreig
 # Created:      2019/02/22
-# Last update:  2019/05/20
+# Last update:  2019/07/18
 
 import requests
 
 
 def get_token(url, email, password):
-    return requests.get(url, headers={"email": email, "password": password}).json()
+    return requests.get(url, headers={"email": email, "password": password}, timeout=10).json()
 
 
 def get_arrive_stop(url, token, stop_id, json):
-    return requests.post(url.replace("<stopId>", stop_id), headers={"accessToken": token}, json=json).json()
+    return requests.post(url.replace("<stopId>", stop_id), headers={"accessToken": token}, json=json, timeout=10).json()
 
 
 def get_bicimad(url, token, station_id):
-    return requests.get(url.replace("<idStation>", station_id), headers={"accessToken": token}).json()
+    return requests.get(url.replace("<idStation>", station_id), headers={"accessToken": token}, timeout=10).json()
 
 
 def get_parkings(url, token):
-    return requests.get(url, headers={"accessToken": token}).json()
+    return requests.get(url, headers={"accessToken": token}, timeout=10).json()

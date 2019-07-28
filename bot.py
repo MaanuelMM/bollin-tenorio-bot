068831e79e69619a2e587e6c79de3c5e511ba3b6
MaanuelMM <manuel.martin.malagon@alumnos.upm.es>
2019-07-28 14:04:03 +0200

    Add metro command functionality and some restructuration

--- bot.py
+++ bot.py
@@ -3,34 +3,32 @@
 # Authors:      MaanuelMM
 # Credits:      eternnoir, atlink, CoreDumped-ETSISI, Eldinnie
 # Created:      2019/02/14
-# Last update:  2019/07/15
+# Last update:  2019/07/28
 
 import os
+import re
 import logger
 import telebot
 import more_itertools
 
-from emt_madrid import get_token, get_arrive_stop, get_bicimad, get_parkings
+from bot_requests import get_token, get_arrive_stop, get_bicimad, get_parkings, get_generic_request
 from data_loader import DataLoader
 from flask import Flask, request
 from flask_sslify import SSLify
+from bs4 import BeautifulSoup
 from logger import get_logger
 
 
-# Enable logger
 logger = get_logger("bot", True)
 logger.info("Starting bot...")
 
 try:
-    # Get data vars
     logger.info("Getting data and config vars...")
     data = DataLoader()
 
-    # Create TeleBot
     logger.info("Creating TeleBot...")
     bot = telebot.TeleBot(token=data.TOKEN)
 
-    # Create Flask server
     logger.info("Creating Flask server...")
     server = Flask(__name__)
 
@@ -48,16 +46,15 @@ def log_message(message):
                 " (ID: " + str(message.from_user.id) + ") " + "[Chat ID: " + str(message.chat.id) + "].")
 
 
-def log_emt_error(response):
-    logger.info("Error in EMT Madrid API request:\n" + response)
+def log_api_error(response):
+    logger.info("Error in API request:\n" + response)
 
 
-def get_text(text):
-    # '/command' and 'the rest of the message'
-    if(len(text.split(" ", 1)) == 1):
-        return ""
-    else:
+def get_text_with_no_command(text):
+    try:
         return text.split(" ", 1)[1]
+    except:
+        return ""
 
 
 def is_integer(string):
@@ -84,8 +81,8 @@ def process_time_left(time_left):
         return "  --:--min"
 
 
+# https://stackoverflow.com/questions/4391697/find-the-index-of-a-dict-within-a-list-by-matching-the-dicts-value
 def sort_arrivals(arrivals):
-    # https://stackoverflow.com/questions/4391697/find-the-index-of-a-dict-within-a-list-by-matching-the-dicts-value
     arrivals_sorted = []
     for arrival in arrivals:
         index_list = list(more_itertools.locate(
@@ -109,8 +106,10 @@ def process_arrival_response(arrivals):
         result += make_arrival_line(arrival)
     return result + "\n\n"
 
+
 def map_link_maker(coordX, coordY):
     return "https://google.com/maps/search/?api=1&query=" + str(coordX) + "," + str(coordY)
+
 
 def bicimad_light(light):
     switcher_light = {
@@ -119,6 +118,7 @@ def bicimad_light(light):
         2: "\U0001F7E5ALTA\U0001F7E5"
     }
     return switcher_light.get(light, "Indefinido")
+
 
 def process_bicimad_response(bicimad):
     line = "\n"
@@ -157,6 +157,29 @@ def process_parkings_response(parkings):
     return result
 
 
+def generate_metro_data_response(data):
+    response = {}
+    soup = BeautifulSoup(data, "html.parser")
+    for img in soup.find_all("img"):
+        line = img.get('class')[0].replace("-", " ").capitalize()
+        estimations = re.sub(r'\s\s+', r'\n    ', img.parent.next_sibling.next_sibling.text).strip()
+        if line not in response:
+            response[line] = []
+        response[line].append(estimations)
+    return response
+
+
+def process_metro_response(data):
+    result = ""
+    for k, v in generate_metro_data_response(data).items():
+        result += "\n"
+        result += k
+        for item in v:
+            result += "\n  "
+            result += item
+    return result
+
+
 def message_sender(message, reply):
     if(len(reply) > 3000):
         splitted_reply = reply.split("\n\n")
@@ -176,23 +199,21 @@ def message_sender(message, reply):
 
 def get_token_clean():
     try:
-        token_response = get_token(
-            data.EMTMADRID_GETTOKENSESSIONURL, data.EMTMADRID_EMAIL, data.EMTMADRID_PASSWORD)
+        token_response = get_token(data.EMTMADRID_GETTOKENSESSIONURL, data.EMTMADRID_EMAIL, data.EMTMADRID_PASSWORD)
         token = token_response["data"][0]["accessToken"]
         del token_response
     except:
         try:
-            log_emt_error(token_response)
+            log_api_error(token_response)
         except:
-            log_emt_error("Unable to get API response.")
+            log_api_error("Unable to get API response.")
         raise
     return token
 
 
 def get_arrive_stop_clean(stop_id):
     try:
-        arrive_stop_response = get_arrive_stop(
-            data.EMTMADRID_GETARRIVESTOPURL, get_token_clean(), stop_id, data.EMTMADRID_GETARRIVESTOPJSON)
+        arrive_stop_response = get_arrive_stop(data.EMTMADRID_GETARRIVESTOPURL, get_token_clean(), stop_id, data.EMTMADRID_GETARRIVESTOPJSON)
         try:
             arrive_stop = arrive_stop_response["data"][0]["Arrive"]
         except:
@@ -200,17 +221,16 @@ def get_arrive_stop_clean(stop_id):
         del arrive_stop_response
     except:
         try:
-            log_emt_error(arrive_stop_response)
+            log_api_error(arrive_stop_response)
         except:
-            log_emt_error("Unable to get API response.")
+            log_api_error("Unable to get API response.")
         raise
     return arrive_stop
 
 
 def get_bicimad_clean(station_id):
     try:
-        bicimad_response = get_bicimad(
-            data.EMTMADRID_GETBICIMADSTATIONSURL, get_token_clean(), station_id)
+        bicimad_response = get_bicimad(data.EMTMADRID_GETBICIMADSTATIONSURL, get_token_clean(), station_id)
         try:
             bicimad = bicimad_response["data"]
         except:
@@ -218,17 +238,16 @@ def get_bicimad_clean(station_id):
         del bicimad_response
     except:
         try:
-            log_emt_error(bicimad_response)
+            log_api_error(bicimad_response)
         except:
-            log_emt_error("Unable to get API response.")
+            log_api_error("Unable to get API response.")
         raise
     return bicimad
 
 
 def get_parkings_clean():
     try:
-        parkings_response = get_parkings(
-            data.EMTMADRID_GETPARKINGSSTATUSURL, get_token_clean())
+        parkings_response = get_parkings(data.EMTMADRID_GETPARKINGSSTATUSURL, get_token_clean())
         try:
             parkings = parkings_response["data"]
         except:
@@ -236,37 +255,59 @@ def get_parkings_clean():
         del parkings_response
     except:
         try:
-            log_emt_error(parkings_response)
+            log_api_error(parkings_response)
         except:
-            log_emt_error("Unable to get API response.")
+            log_api_error("Unable to get API response.")
         raise
     return parkings
 
-# Start command handler
+
+def get_metro_arrival_clean(station_id):
+    try:
+        metro_response = get_generic_request(data.METROMADRID_GETARRIVESTATIONURL + station_id).json()
+        try:
+            metro = metro_response[0]["data"]
+        except:
+            metro = ""
+        del metro_response
+    except:
+        log_api_error("Unable to get API response due to unauthorized access.")
+        raise
+    return metro
+
+
+def get_metro_stations_clean():
+    try:
+        return get_generic_request(data.METROMADRID_GETSTATIONSLISTURL).json()
+    except:
+        log_api_error("Unable to get Metro Madrid stations from GitHub.")
+        raise
+
+
+
 @bot.message_handler(commands=['start'])
 def send_start(message):
     log_message(message)
     bot.reply_to(message, data.START)
 
-# Hola command handler
+
 @bot.message_handler(commands=['hola'])
 def send_hola(message):
     log_message(message)
     bot.reply_to(message, data.HOLA + message.from_user.first_name)
 
-# Parada command handler
+
 @bot.message_handler(commands=['parada'])
 def send_parada(message):
     log_message(message)
-    text = get_text(message.text)
+    text = get_text_with_no_command(message.text)
     if(str(message.chat.id) in data.PARADA_CHAT_LIST and text.upper() in data.PARADA_CHAT_LIST[str(message.chat.id)]):
         text = data.PARADA_CHAT_LIST[str(message.chat.id)][text.upper()]
     if is_integer(text):
         try:
             arrive_stop = get_arrive_stop_clean(text)
             if arrive_stop:
-                message_sender(message, data.PARADA_SUCCESSFUL.replace(
-                    "<stopId>", text) + process_arrival_response(arrive_stop) + data.PARADA_SUCCESSFUL_DISCLAIMER)
+                message_sender(message, data.PARADA_SUCCESSFUL.replace("<stopId>", text) + process_arrival_response(arrive_stop) + data.PARADA_SUCCESSFUL_DISCLAIMER)
             else:
                 bot.reply_to(message, data.PARADA_NO_ESTIMATION)
             del arrive_stop
@@ -277,11 +318,11 @@ def send_parada(message):
         bot.reply_to(message, data.PARADA_BAD_SPECIFIED)
     del text
 
-# Bicimad command handler
+
 @bot.message_handler(commands=['bicimad'])
 def send_bicimad(message):
     log_message(message)
-    text = get_text(message.text)
+    text = get_text_with_no_command(message.text)
     if is_integer(text):
         try:
             bicimad = get_bicimad_clean(text)
@@ -297,26 +338,49 @@ def send_bicimad(message):
         bot.reply_to(message, data.BICIMAD_BAD_SPECIFIED)
     del text
 
-# Parkings command handler
+
 @bot.message_handler(commands=['parkings'])
 def send_parkings(message):
     log_message(message)
     try:
-        message_sender(message, data.PARKINGS +
-                       process_parkings_response(get_parkings_clean()))
+        message_sender(message, data.PARKINGS + process_parkings_response(get_parkings_clean()))
     except Exception as e:
         logger.error(e, exc_info=True)
         bot.reply_to(message, data.REQUEST_FAIL)
 
 
-# Help command handler
+@bot.message_handler(commands=['metro'])
+def send_metro(message):
+    log_message(message)
+    text = get_text_with_no_command(message.text)
+    if text:
+        try:
+            stations = get_metro_stations_clean()
+            if text in stations:
+                response = get_metro_arrival_clean(stations[text])
+                if response:
+                    message_sender(message, process_metro_response(data))
+                else:
+                    bot.reply_to(message, data.METRO_NO_ESTIMATION)
+                del response
+            else:
+                bot.reply_to(message, data.METRO_DOES_NOT_EXIST)
+            del stations
+        except Exception as e:
+            logger.error(e, exc_info=True)
+            bot.reply_to(message, data.REQUEST_FAIL)
+    else:
+        bot.reply_to(message, data.METRO_BAD_SPECIFIED)
+    del text
+
+
 @bot.message_handler(commands=['help'])
 def send_help(message):
     log_message(message)
     bot.reply_to(message, data.HELP)
 
 
-@server.route('/' + data.TOKEN, methods=['POST'])
+@server.route("/" + data.TOKEN, methods=['POST'])
 def getMessage():
     bot.process_new_updates(
         [telebot.types.Update.de_json(request.stream.read().decode("utf-8"))])
@@ -325,11 +389,9 @@ def getMessage():
 
 @server.route("/")
 def webhook():
-    # Remove current webhook
     logger.info("Removing current webhook...")
     bot.remove_webhook()
     logger.info("Successfully removed current webhook.")
-    # Create new webhook
     logger.info("Creating new webhook...")
     bot.set_webhook(url=data.URL+data.TOKEN)
     logger.info("Successfully created new webhook.")
@@ -338,7 +400,6 @@ def webhook():
 
 if __name__ == "__main__":
     try:
-        # Run server
         logger.info("Starting running server...")
         server.run(host="0.0.0.0", port=int(data.PORT))
     except Exception as e:
